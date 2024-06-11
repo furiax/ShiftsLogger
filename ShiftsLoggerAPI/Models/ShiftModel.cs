f1a8480d11a10493e737d7eb6f0d8eb52b278895
furiax <carlmalfliet@proximus.be>
2024-06-11 10:48:44 +0200

    Add project files.

---
+++ ShiftsLoggerAPI/Models/ShiftModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ShiftsLoggerAPI.Models
+{
+    public class ShiftModel
+    {
+        public int Id { get; set; }
+        public string EmployeeName { get; set; } = string.Empty;
+        public DateTime StartOfShift { get; set; }
+        public DateTime EndOfShift { get; set; }
+        public TimeSpan Duration => EndOfShift - StartOfShift;
+    }
+}
